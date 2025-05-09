bfce16f7e8dac31052c8653fca0f0cdee2303c6d
Joel72Polo <jpolocaicedo@gmail.com>
2025-05-09 15:29:15 -0500

    Update cli_view.py

--- cli_view.py
+++ cli_view.py
@@ -1,9 +1,10 @@
-# app/views/cli_view.py
-
 class CLIView:
     def mostrar_bienvenida(self):
         print("\n=== PROYECTO DEPARTAMENTOS Y MUNICIPIOS DE COLOMBIA ===")
 
     def mostrar_datos(self, lista_datos):
-        for item in lista_datos:
-            print(f"{item.get('departamento')} - {item.get('municipio')}")
+        if lista_datos:
+            for item in lista_datos:
+                print(f"{item.get('departamento', 'N/A')} - {item.get('municipio', 'N/A')}")
+        else:
+            print("No se encontraron datos para mostrar.")
